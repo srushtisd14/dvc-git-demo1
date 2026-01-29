c121f93b65b3f5e3a076b70e0e201ec03c5e6146
srushtisd14 <srushtisd14@gmail.com>
2026-01-29 22:28:05 +0530

    Fix DVC pipeline and generate output inside script

--- read.py
+++ read.py
@@ -1,5 +1,17 @@
 import pandas as pd
 
 df = pd.read_csv("data.csv")
-print("Shape:", df.shape)
-print(df.head())
+
+output = []
+output.append(f"Shape: {df.shape}\n")
+output.append("First 5 rows:\n")
+output.append(df.head().to_string())
+
+result = "\n".join(output)
+
+print(result)
+
+# DVC output
+with open("output.txt", "w") as f:
+    f.write(result)
+
